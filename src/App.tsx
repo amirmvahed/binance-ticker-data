d2e12c0aad783c6a70cbb35d36a7d0b55062b0e4
amirmvahed <amirmvahed@gmail.com>
2024-07-11 11:04:07 -0400

    feat(TickerTable.tsx): Created UI table for showing tickers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import TickerTable from './components/TickerTable';
+import './App.css'
 
 const App: React.FC = () => {
     return (
         <div>
-            <h1 style={{textAlign: 'center', color: 'white', margin: '10px 0'}}>Binance Ticker Data</h1>
+            <h1 className={'header'}>Binance Ticker Data</h1>
             <TickerTable/>
         </div>
     );
--- src/components/TickerTable.tsx
+++ src/components/TickerTable.tsx
@@ -1,11 +1,108 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
+import styled from 'styled-components'
+
+const Container = styled.div`
+  border-radius: 8px;
+  background-color: #212630;
+  color: white;
+  padding: 8px;
+  max-width: 500px;
+  margin: auto;
+  height: 90vh;
+  overflow-y: auto;
+  width: 100%;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+
+  &::-webkit-scrollbar {
+    width: 7px;
+    border-radius: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+`
+
+const Table = styled.table`
+  width: 100%;
+`
+
+const Tr = styled.tr`
+  &:hover {
+    background: #29303d;
+    cursor: pointer;
+  }
+`
+
+const Td = styled.td`
+  border: 0;
+  padding: 4px 8px;
+  font-size: 1.4rem;
+
+  &:nth-child(even) {
+    text-align: right;
+    width: 40%
+  }
+`
+
+const Text = styled.span`
+  display: inline-block;
+  margin: 8px 0;
+  color: gray;
+  font-size: 1rem;
+`
+
+const PriceChange = styled('span')<{ positive: boolean }>`
+  color: ${({positive}) => (positive ? 'green' : 'red')};
+  margin: 8px 0;
+  display: inline-block;
+  font-size: 1rem;
+`
+
+type Ticker = {
+    s: string; // Symbol
+    c: string; // Last price
+    P: string; // Price change percent
+}
 
 const TickerTable: React.FC = () => {
+    const [tickers, setTickers] = useState<Ticker[]>([])
+
 
     return (
-        <Fragment>
-            Table
-        </Fragment>
+        <Container>
+            <tbody>
+            {
+                tickers.map(ticker => {
+                    return (
+                        <Tr key={ticker.s}>
+                            <Td>
+                                {ticker.s}
+                                <br/>
+                                <Text>
+                                    Perpetual
+                                </Text>
+                            </Td>
+                            <Td>
+                                {ticker.c}
+                                <br/>
+                                <PriceChange positive={parseFloat(ticker.P) >= 0}>
+                                    {ticker.P}%
+                                </PriceChange>
+                            </Td>
+                        </Tr>
+                    )
+                })
+            }
+            </tbody>
+        </Container>
     );
 };
 
