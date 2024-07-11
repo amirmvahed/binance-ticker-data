079467896c159a1cc58e82230f3be9b6c0fa8a1d
amirmvahed <amirmvahed@gmail.com>
2024-07-11 09:10:29 -0400

    feat(App.tsx): First settings and add favicon

---
+++ src/components/TickerTable.tsx
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+
+const TickerTable: React.FC = () => {
+
+    return (
+        <Fragment>
+            Table
+        </Fragment>
+    );
+};
+
+export default TickerTable;
